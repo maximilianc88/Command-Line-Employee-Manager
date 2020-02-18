f9d4e6d5c4cb6b6f9cf13a05dddecec55eba9398
Maximillian Cartwright <maximilianc@zillowgroup.com>
2020-02-17 20:49:19 -0800

    schema update

--- dbSchema.sql
+++ dbSchema.sql
@@ -22,8 +22,8 @@ CREATE TABLE employee(
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id int,
-  manager_id int,
+  role_id int NOT NULL,
+  manager_id int NULL,
   FOREIGN KEY (role_id) REFERENCES role(id),
   FOREIGN KEY (manager_id) REFERENCES employee(id),
   PRIMARY KEY (id)
