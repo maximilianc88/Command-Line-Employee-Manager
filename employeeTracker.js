23caf9d3cf3eca6abea2b93a89c311d67635f4c1
Maximillian Cartwright <maximilianc@zillowgroup.com>
2020-02-23 21:16:47 -0800

    added additional add functions

--- employeeTracker.js
+++ employeeTracker.js
@@ -8,17 +8,43 @@ const consoleTable = require("console.table");
 
 function table(message, response) {
   return console.table(message, response, "press any key");
-};
+}
 
 const promptMessages = {
   viewAll: "View all employees",
   viewAllDpt: "View all departments",
   viewAllRole: "View all roles",
   addNewb: "Add employee",
-  addRole: "Remove employee",
-  addDept: "Update employee role",
+  addRole: "Add role",
+  addDept: "Add Department",
   exit: "Later gator!"
 };
+
+const deptMessages = 
+  {
+      type: 'rawlist',
+      name: 'departmentz',
+      message: 'What would you like to do?',
+      choices: ['View Departments', 'Add Departments', 'Delete Departments', 'Go Back']
+  };
+
+  const roleMessages = 
+  {
+      type: 'rawlist',
+      name: 'rolez',
+      message: 'What would you like to do?',
+      choices: ['View Departments', 'Add Departments', 'Delete Departments', 'Go Back']
+  };
+
+  const employeeMessages = 
+  {
+      type: 'rawlist',
+      name: 'employeez',
+      message: 'What would you like to do?',
+      choices: ['View Departments', 'Add Departments', 'Delete Departments', 'Go Back']
+  };
+
+
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -26,7 +52,7 @@ const connection = mysql.createConnection({
   port: 3306,
 
   user: "root",
- 
+
   password: "penguin",
   database: "employeeTracker_DB"
 });
@@ -39,8 +65,7 @@ connection.connect(err => {
 
 function table(message, response) {
   return console.table(message, response);
-};
-
+}
 
 function prompt() {
   inquirer
@@ -48,7 +73,15 @@ function prompt() {
       name: "action",
       type: "rawlist",
       message: "What would you like to do?",
-      choices: [promptMessages.viewAll, promptMessages.viewAllDpt, promptMessages.viewAllRole, promptMessages.exit]
+      choices: [
+        promptMessages.viewAll,
+        promptMessages.viewAllDpt,
+        promptMessages.viewAllRole,
+        promptMessages.addNewb,
+        promptMessages.addRole,
+        promptMessages.addDept,
+        promptMessages.exit
+      ]
     })
     .then(answer => {
       console.log(answer);
@@ -58,38 +91,168 @@ function prompt() {
           prompt();
           break;
 
-          case promptMessages.viewAllDpt:
+        case promptMessages.viewAllDpt:
+          viewAllDpt();
+          prompt();
+          break;
+
+        case promptMessages.viewAllRole:
+          viewAllRole();
+          prompt();
+          break;
+
+        case promptMessages.addNewb:
+          inquirer.prompt([
+            {
+                type: 'input',
+                name: 'first_name',
+                message: 'First name',
+            },
+            {
+                type: 'input',
+                name: 'last_name',
+                message: 'Last name',
+            },
+            {
+                type: 'list',
+                name: 'role_id',
+                message: `Role ID: \n Choose 1 for Sales Lead \n Choose 2 for Sales Person \n Choose 3 for Lead Engineer \n Choose 4 for Software Engineer \n Choose 5 for Accountant \n Choose 6 for Legal Team Lead \n Choose 7 for Lawyer`,
+                choices:[1, 2, 3, 4, 5, 6, 7]
+            },
+            {
+                type: 'list',
+                name: 'manager_id',
+                message: `Manager ID: \nChoose 1 for Bryan Narciso \n Choose 4 for Fatin Mohamed \n Choose 3 for Lyana Zainuddin`,
+                choices:['null',1, 4, 6]
+            }
+        ]).then(answers => {
+            let first_name = answers.first_name;
+            let last_name = answers.last_name;
+            let role_id = answers.role_id;
+            let manager_id = JSON.parse(answers.manager_id);
+            addNewb(first_name, last_name, role_id, manager_id);
+            viewAll();
+            prompt();
+        });
+        break;
+
+        case promptMessages.addRole:
+          inquirer.prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'Title',
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: 'Salary',
+            },
+            {
+                type: 'list',
+                name: 'department_id',
+                message: `Department ID: \n Choose 1 for Sales \n Choose 2 for Engineering \n Choose 3 for Finance \n Choose 4 for Legal`,
+                choices:[1, 2, 3, 4]
+            }
+        ]).then(answers => {
+            let title = answers.title;
+            let salary = answers.salary;
+            let department_id = answers.department_id;
+            addRole(title, salary, department_id);
+            viewAllRole();
+            prompt();
+        });
+          break;
+
+        case promptMessages.addDept:
+          inquirer.prompt([
+            {
+                type: 'input',
+                name: 'deptName',
+                message: 'New department name:',
+            }
+        ]).then(answers => {
+            let name = answers.deptName;
+            addDpt(name);
             viewAllDpt();
             prompt();
-            break;
+        });
+          break;
 
-            case promptMessages.viewAllRole:
-              viewAllRole();
-              prompt();
-              break;
+        case promptMessages.exit:
+          connection.end();
+          break;
+      }
+    });
+}
 
-          case promptMessages.exit:
-            connection.end();
-            break;
-        }})};
-
-
+//view functions
 
 function viewAll() {
-  const query = connection.query(`SELECT employee.id, first_name, last_name, title, department.name, salary, manager_id FROM department join role ON department.id = role.department_id
-  join employee ON role.id = employee.role_id order by employee.id asc;`, (err, res)=>{
+  const query = connection.query(
+    `SELECT employee.id, first_name, last_name, title, department.name, salary, manager_id FROM department join role ON department.id = role.department_id
+  join employee ON role.id = employee.role_id order by employee.id asc;`,
+    (err, res) => {
       if (err) throw err;
-      console.table("View Employees", res,'Press any key to continue');
-  });}
+      console.table("View Employees", res, "Press any key to continue");
+    }
+  );
+}
 
-  function viewAllDpt() {
-    const query = connection.query( "SELECT * FROM department", (err, res)=>{
-      if (err) throw err;
-      console.table("View Departments", res,'Press any key to continue');
-    });}
+function viewAllDpt() {
+  const query = connection.query("SELECT * FROM department", (err, res) => {
+    if (err) throw err;
+    console.table("View Departments", res, "Press any key to continue");
+  });
+}
 
-    function viewAllRole() {
-      const query = connection.query("SELECT * FROM role", (err, res)=>{
-        if (err) throw err;
-        console.table("View Roles", res,'Press any key to continue');
-      });}
+function viewAllRole() {
+  const query = connection.query("SELECT * FROM role", (err, res) => {
+    if (err) throw err;
+    console.table("View Roles", res, "Press any key to continue");
+  });
+}
+
+//add functions
+function addDpt(name) {
+  const query = connection.query(
+      "INSERT INTO department SET ?",
+      { name: name },
+      function(err, res) {
+          if (err) throw err;
+          let message = "View Departments";
+          table(message, res);
+      }
+  );
+}
+
+function addRole(title, salary, department_id) {
+  const query = connection.query(
+      "INSERT INTO role SET ?",
+      {
+          title: title,
+          salary: salary,
+          department_id: department_id
+      },
+      function(err, res) {
+          if (err) throw err;
+          console.table("View Roles", res, "Press any key to continue");
+      }
+  )
+}
+
+function addNewb(first_name, last_name, role_id, manager_id) {
+  const query = connection.query(
+      "INSERT INTO employee SET ?",
+      {
+          first_name: first_name,
+          last_name: last_name,
+          role_id: role_id,
+          manager_id: manager_id
+      },
+      function(err, res) {
+          if (err) throw err;
+          console.table('View Employees', res, "Press any key to continue");
+      }
+  )
+}
